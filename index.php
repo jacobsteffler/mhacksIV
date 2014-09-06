a800461140912fff990ed36564951bc4e49f0617
Ajaay Chandrasekaran <ajaay@umich.edu>
2014-09-06 15:41:36 -0400

    Complete most of index.php

--- index.php
+++ index.php
@@ -17,21 +17,36 @@
     $i++;//i stores number of puzzles + + button
 
     //String html representation
-    $table= "<table width='600' cellpadding ='5' cellspacing ='5' border='1'>";
+    $table= "<table width='525' cellpadding ='0' cellspacing ='0' border='0'>";
 
     //Concatenation of tr and td elements
        $num_rows=(i-1)/3+1
        $num_columns=3
 
-       for($j=1; j<=i;++j){
-	   if(j==1){
-		   tableTag(table,o, r);
-		   table .= <Element with plus.png>
-		   tableTag(table,c,r)
+       for($j=1; $j<=$i;++$j){
+	   if($j==1){
+		   tableTag(table,"o", "r");
+		   tableTag(table,"o","c");
+		   table .= "<img src= \"plus.png\"";		   
 	   }
+	   else{
+		   tableTag(table,"o","c");
+		   table .= "<img src= \" . file_names[j-2]."/"."thumbnail.png" . \" ";
+	   }
+	   tableTag(table,"c","c");
 
-	}
+	   //Close the row. Start new row
+	   if($j % num_columns==0){
+		tableTag(table,"c","r");
+		if($j != $i){
+		      tableTag(table,"o","r");
+		}
 
+	   }
+           //Create placeholders to create/close more columns if necessary?
+       }//End for loop
+	   
+	 
 
        table .= "</table>"
        //Display the table
